e2da621a7be7b88469b3fc690cdda8ef98404540
demostanis <demostanis@protonmail.com>
2024-11-11 00:02:14 +0100

    setup basic argocd

--- p3/test.sh
+++ p3/test.sh
@@ -2,16 +2,6 @@
 
 k='sudo kubectl'
 
-is_ready() {
-	[ $($k get -n dev deployment | awk 'NR==2{print$2}') = 1/1 ]
-}
-
-delay() {
-	sleep 1
-}
-
-until is_ready; do delay; done
-
 attempts=0
 # give some time for the container to actually start...
 while ! curl -s localhost:8888 | grep -q '"status":"ok"'; do
@@ -20,5 +10,5 @@ while ! curl -s localhost:8888 | grep -q '"status":"ok"'; do
 		exit 1
 	fi
 	(( attempts+=1 ))
-	delay
+	sleep 1
 done
