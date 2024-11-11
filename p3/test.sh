89e99daed8a3a14fae17128aa1651f40beea9ae8
demostanis <demostanis@protonmail.com>
2024-11-11 01:09:53 +0100

    create the app in argocd

--- p3/test.sh
+++ p3/test.sh
@@ -3,12 +3,21 @@
 k='sudo kubectl'
 
 attempts=0
-# give some time for the container to actually start...
-while ! curl -s localhost:8888 | grep -q '"status":"ok"'; do
-	if (( attempts > 60 )); then
-		echo something went wrong, the app was not correctly deployed
-		exit 1
+while :; do
+	# give some time for the container to actually start...
+	playground_test=$(curl -s localhost:8888 | grep -q '"status":"ok"';
+		echo $?)
+	argocd_test=$(curl -s localhost:8888/argo-cd/ | grep -q '<title>Argo';
+		echo $?)
+
+	if [ "$playground_test" != 0 -o "$argocd_test" != 0 ]; then
+		if (( attempts > 120 )); then
+			echo something went wrong, the app was not correctly deployed
+			exit 1
+		fi
+		(( attempts+=1 ))
+		sleep 1
+	else
+		break
 	fi
-	(( attempts+=1 ))
-	sleep 1
 done
